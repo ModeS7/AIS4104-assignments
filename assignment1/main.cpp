6b6dc7ab8899dd934318b8208908d2e87cee12d2
Modestas <modesuka@gmail.com>
2024-08-28 16:25:26 +0200

    task 2.1 is complete

--- assignment1/main.cpp
+++ assignment1/main.cpp
@@ -2,19 +2,29 @@
 
 #include <Eigen/Dense>
 
-void example(double constant)
+// 2.1 task1a
+Eigen::Matrix3d skew_symmetric(Eigen::Vector3d v)
 {
-    Eigen::Matrix3d identity;
-    identity <<
-        1.0, 0.0, 0.0,
-        0.0, 1.0, 0.0,
-        0.0, 0.0, 1.0;
-    std::cout << "I: " << std::endl << identity << std::endl << std::endl;
-    std::cout << constant <<"*I: " << std::endl << constant * identity << std::endl << std::endl;
+    Eigen::Matrix3d skew_matrix;
+    skew_matrix <<
+        0.0, -v(2), v(1),
+        v(2), 0.0, -v(0),
+        -v(1), v(0), 0.0;
+    return skew_matrix;
+}
+
+// 2.1 task1b
+void skew_symmetric_test()
+{
+    Eigen::Matrix3d skew_matrix = skew_symmetric(Eigen::Vector3d{0.5, 0.5, 0.707107});
+    std::cout << "Skew-symmetric matrix: " << std::endl;
+    std::cout << skew_matrix << std::endl;
+    std::cout << "Skew-symmetric matrix transposition: " << std::endl;
+    std::cout << -skew_matrix.transpose() << std::endl;
 }
 
 int main()
 {
-    example(2.0);
+    skew_symmetric_test();
     return 0;
 }
