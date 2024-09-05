05d4e7cfc83f8f8b1d4eaa45b833a57f41581be8
Modestas <modesuka@gmail.com>
2024-09-05 21:35:00 +0200

    fixed few issues

--- assignment2/main.cpp
+++ assignment2/main.cpp
@@ -5,54 +5,13 @@
 
 using namespace math;
 
+
+
 bool floatEquals(double a, double b)
 {
     return std::abs(a - b) < 1e-6;
 }
 
-double deg_to_rad = EIGEN_PI / 180.0;
-
-double rad_to_deg = 57.2957795;
-
-
-Eigen::Matrix3d rotate_x(double radians)
-{
-    Eigen::Matrix3d matrix;
-    matrix <<
-           1.0, 0.0, 0.0,
-            0.0, std::cos(radians), -std::sin(radians),
-            0.0, std::sin(radians), std::cos(radians);
-    return matrix;
-}
-
-
-Eigen::Matrix3d rotate_y(double radians)
-{
-    Eigen::Matrix3d matrix;
-    matrix <<
-           std::cos(radians), 0.0, std::sin(radians),
-            0.0, 1.0, 0.0,
-            -std::sin(radians), 0.0, std::cos(radians);
-    return matrix;
-}
-
-
-Eigen::Matrix3d rotate_z(double radians)
-{
-    Eigen::Matrix3d matrix;
-    matrix <<
-           std::cos(radians), -std::sin(radians), 0.0,
-            std::sin(radians), std::cos(radians), 0.0,
-            0.0, 0.0, 1.0;
-    return matrix;
-}
-Eigen::Matrix3d rotation_matrix_from_euler_zyx(const Eigen::Vector3d &e)
-{
-    Eigen::Matrix3d matrix;
-    Eigen::Matrix3d I = Eigen::Matrix3d::Identity();
-    matrix = I * rotate_z(e(0)) * rotate_y(e(1)) * rotate_x(e(2));
-    return matrix;
-}
 Eigen::Vector3d euler_zyx_from_rotation(const Eigen::Matrix3d &r)
 {
     double a = 0.0;
@@ -81,14 +40,12 @@ Eigen::Vector3d euler_zyx_from_rotation(const Eigen::Matrix3d &r)
 }
 
 
-
-
 int main()
 {
-    Eigen::Vector3d e = Eigen::Vector3d(60.0, 45.0, 30.0) * deg_to_rad;
+    Eigen::Vector3d e = Eigen::Vector3d(60.0, 45.0, 30.0) * math::deg_to_rad;
     Eigen::Matrix3d rotation_matrix= math::rotation_matrix_from_euler_zyx(e);
     Eigen::Vector3d ea = euler_zyx_from_rotation(rotation_matrix);
-    std::cout << " E:" << e.transpose() * rad_to_deg << std::endl;
-    std::cout << "Ea:" << ea.transpose() * rad_to_deg << std::endl;
+    std::cout << " E:" << e.transpose() * math::rad_to_deg << std::endl;
+    std::cout << "Ea:" << ea.transpose() * math::rad_to_deg << std::endl;
     return 0;
 }
